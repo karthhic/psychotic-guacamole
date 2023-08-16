95a6b5e57a63b4bfbc3e5a618c0526fd8c00d5e5
Karthhic <karthhic@gmail.com>
2023-08-16 03:59:29 -0400

    added admin

--- server.js
+++ server.js
@@ -63,5 +63,35 @@ if (req.url === '/info') {
 //---------------------------------------End of info location process
 
 
+//-----------Start of info section-------
+
+//---------------------------------------info location start
+if (req.url === '/admin') {
+    //---------------------------------------Post method process for info
+            if (req.method === 'POST') {
+    //---------------------------------------Read file and send it as response
+                fs.readFile('./pages/post.html', function(err, data) {
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.write(data);
+                res.write("/info page method = post")
+                return res.end();
+              });
+            }
+    //---------------------------------------End of post method process for info
+        
+    //---------------------------------------Get method process for info
+            if (req.method === 'GET') {
+    //---------------------------------------Read file and send it as response
+                fs.readFile('./pages/home.html', function(err, data) {
+                    res.writeHead(200, {'Content-Type': 'text/html'});
+                    res.write(data);
+                    res.write("/info page method = get")
+                    return res.end();
+                  });
+            }
+    //---------------------------------------end of get method process for info
+        }
+    //---------------------------------------End of info location process
+
 
 }).listen(80);
